48feb496e1424878a0507880488889291a723849
thomas_huchede <huchede.thomas@gmail.com>
2016-04-20 16:11:07 +0200

    Change base User model (too rigid) for SITUser model

--- server/boot/health.js
+++ server/boot/health.js
@@ -1,17 +1,7 @@
 module.exports = function(server) {
   var router = server.loopback.Router();
-  router.get('/Users/health', function(req, res) {
-    res.status(200).send('OK');
+  router.get('/SITUsers/health', function(req, res) {
+    res.status(200).send({status:'OK'});
   });
-  /*
-  router.post('/Users/health' , function(req, res) {
-    try{
-      res.status(200).send('Hello'+req.body.email);
-    }catch(e){
-      console.log('sup');
-      console.log(e);
-    }
-  });
-  */
   server.use(router);
 };
--- test/test.js
+++ test/test.js
@@ -2,29 +2,29 @@
 /*
  {"email":"foo@bar.com", "password":"xxx"}
  */
-var request = require('request');
+//var request = require('request');
 var supertest = require('supertest');
 var expect = require('chai').expect;
 
+var server = supertest('http://localhost:3000/SITUsers');
+
 describe('Server health response', function () {
   it('should return 200 OK', function (done) {
-    request.get('http://localhost:3000/Users/health',
-      function (err, res, body) {
+    server.get('/health')
+      .end(function (err, res) {
         if (err) {
           throw err;
         }
         expect(res.statusCode).to.equal(200);
-        expect(body).to.equal('OK');
+        expect(res.body.status).to.equal('OK');
         done();
       });
   });
 });
 
-var server = supertest('http://localhost:3000');
-
 describe('Sign up a user', function () {
   it('should return user email and id', function (done) {
-    server.post('/Users')
+    server.post('/')
       .send({'email': 'foo@bar.com', 'password': 'xxx'})
       .end(function (err, res) {
         if (err) {
@@ -42,7 +42,7 @@ var token;
 
 describe('Log in with the user 1', function () {
   it('should return a token for user 1', function (done) {
-    server.post('/Users/login')
+    server.post('/login')
       .send({'email': 'foo@bar.com', 'password': 'xxx'})
       .end(function (err, res) {
         if (err) {
@@ -61,7 +61,7 @@ describe('Log in with the user 1', function () {
 
 describe('Get user 1', function () {
   it('should return email and id of user 1', function (done) {
-    server.get('/Users/1')
+    server.get('/1')
       .query('access_token='+token)
       .end(function (err, res) {
         if (err) {
