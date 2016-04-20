666a0b46b81054de9fbba407d5a723b4ea5440c0
thomas_huchede <huchede.thomas@gmail.com>
2016-04-20 15:40:59 +0200

    Update test

--- test/test.js
+++ test/test.js
@@ -1,4 +1,9 @@
+/* jshint expr:true */
+/*
+ {"email":"foo@bar.com", "password":"xxx"}
+ */
 var request = require('request');
+var supertest = require('supertest');
 var expect = require('chai').expect;
 
 describe('Server health response', function () {
@@ -15,18 +20,77 @@ describe('Server health response', function () {
   });
 });
 
-/*describe('Sign up a user', function () {
+var server = supertest('http://localhost:3000');
+
+describe('Sign up a user', function () {
   it('should return user email and id', function (done) {
-    request.post('http://localhost:3000/Users',
-      JSON.stringify({'email': 'foo@bar.baz', 'password': 'xxx'}))
-      .end(function (err, res, body) {
+    server.post('/Users')
+      .send({'email': 'foo@bar.com', 'password': 'xxx'})
+      .end(function (err, res) {
         if (err) {
           throw err;
         }
         expect(res.statusCode).to.equal(200);
-        expect(body.email).to.equal('foo@bar.baz');
-        expect(body.id).to.equal(1);
+        expect(res.body.email).to.equal('foo@bar.com');
+        expect(res.body.id).to.equal(1);
         done();
       });
   });
-});*/
+});
+
+var token;
+
+describe('Log in with the user 1', function () {
+  it('should return a token for user 1', function (done) {
+    server.post('/Users/login')
+      .send({'email': 'foo@bar.com', 'password': 'xxx'})
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.ttl).to.exist;
+        expect(res.body.created).to.exist;
+        expect(res.body.id).to.exist;
+        token = res.body.id;
+        expect(res.body.userId).to.equal(1);
+        done();
+      });
+  });
+});
+
+describe('Get user 1', function () {
+  it('should return email and id of user 1', function (done) {
+    server.get('/Users/1')
+      .query('access_token='+token)
+      .end(function (err, res) {
+        if (err) {
+          throw err;
+        }
+        expect(res.statusCode).to.equal(200);
+        expect(res.body.email).to.exist;
+        expect(res.body.email).to.equal('foo@bar.com');
+        expect(res.body.id).to.exist;
+        expect(res.body.id).to.equal(1);
+        done();
+      });
+  });
+});
+
+/*
+ describe('Get user 1', function () {
+ it('should return email and id of user 1', function (done) {
+ server.get('/Users/1')
+ .query('access_token='+token)
+ .end(function (err, res) {
+ if (err) {
+ throw err;
+ }
+ expect(res.statusCode).to.equal(200);
+ expect(res.body.exists).to.exist;
+ expect(res.body.exists).to.equal(true);
+ done();
+ });
+ });
+ });
+ */
