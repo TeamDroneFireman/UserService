92a252c4f0a2daff085669ac8cc12928131c6334
thomas_huchede <huchede.thomas@gmail.com>
2016-04-21 10:59:06 +0200

    Modified tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,26 @@ var gulp = require('gulp');
 var mocha = require('gulp-mocha');
 var bg = require('gulp-bg');
 var wait = require('gulp-wait');
+var run = require('gulp-run');
 
 var bgstart;
 
 gulp.task('start', bgstart = bg('node', '.'));
 
 gulp.task('test', ['start'], function () {
+  return gulp.src('./test/test.js', {read: true}).pipe(wait(1500))
+    .pipe(run('mocha --reporter xunit > report-mocha.xml'))
+    .once('end', function () {
+      bgstart.setCallback(function () { process.exit(0); });
+      bgstart.stop(0);
+    })
+    .once('error', function () {
+      bgstart.setCallback(function () { process.exit(0); });
+      bgstart.stop(0);
+    });
+});
+
+gulp.task('testdev', ['start'], function () {
   return gulp.src('./test/test.js', {read: true}).pipe(wait(1500))
     .pipe(mocha({reporter: 'nyan'}))
     .once('end', function () {
