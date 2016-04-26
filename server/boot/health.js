22a8582c35c72a931c47756e40162c747b41160c
thomas_huchede <huchede.thomas@gmail.com>
2016-04-26 17:52:11 +0200

    Merging...

--- server/boot/health.js
+++ server/boot/health.js
@@ -1,6 +1,6 @@
 module.exports = function(server) {
   var router = server.loopback.Router();
-  router.get('/SITUsers/health', function(req, res) {
+  router.get('/api/SITUsers/health', function(req, res) {
     res.status(200).send({status:'OK'});
   });
   server.use(router);
